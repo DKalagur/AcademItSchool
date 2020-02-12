7a048ff0e9067c4b9648b12e30df477a2be99f6c
DKalagur <kalagurde@gmail.com>
2020-02-12 20:36:53 +0700

    Task "Shape" Part1

---
+++ Shapes/src/ru/academit/kalagur/shapes/Circle.java
@@ -0,0 +1,25 @@
+package ru.academit.kalagur.shapes;
+
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getWidth() {
+        return 2 * radius;
+    }
+
+    public double getHeight() {
+        return 2 * radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+}
---
+++ Shapes/src/ru/academit/kalagur/shapes/Rectangle.java
@@ -0,0 +1,29 @@
+package ru.academit.kalagur.shapes;
+
+public class Rectangle implements Shape {
+    private double height;
+    private double width;
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getArea() {
+        return height * width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (height + width);
+    }
+}
+
+
