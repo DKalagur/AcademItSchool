dce00433d85ff8cad606494d97192a286e0bc493
Денис <kalagurde@gmail.com>
2020-03-23 18:25:20 +0700

    Задача "ArrayListHome" Исправление замечаний

--- ArrayListHome/src/ru/akademit/kalagur/ArrayListHomeMain.java
+++ ArrayListHome/src/ru/akademit/kalagur/ArrayListHomeMain.java
@@ -1,24 +1,25 @@
 package ru.akademit.kalagur;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArrayListHome_Main {
+public class ArrayListHomeMain {
 
     //первая подзадача
-    public static void main(String[] args) throws FileNotFoundException {
-        try (Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\TEMP\\Desktop\\текст.txt"), "windows-1251")) {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(new FileInputStream("текст.txt"), "windows-1251")) {
             ArrayList<String> lines = new ArrayList<>();
 
             while (scanner.hasNextLine()) {
-                String x = scanner.nextLine();
-                lines.add(x);
+                lines.add(scanner.nextLine());
             }
 
             System.out.println(lines);
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла");
         }
 
         //вторая подзадача
@@ -35,7 +36,7 @@ public class ArrayListHome_Main {
 
         //третья подзадача
         ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(4, -1, 1, 3, 1, 0, 3, 4, 0, 8, 8));
-        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>(list1.size());
 
         for (Integer e : list1) {
             if (!list2.contains(e)) {
