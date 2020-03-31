7305c14ecbf46e7084ec1005874cdcaa6c436538
Денис Калагур <kalagurde@gmail.com>
2020-03-31 12:29:57 +0700

    Задача "List" Исправление замечаний

--- List/src/ru/academit/kalagur/list/SinglyLinkedList.java
+++ List/src/ru/academit/kalagur/list/SinglyLinkedList.java
@@ -4,42 +4,42 @@ public class SinglyLinkedList<T> {
     private ListItem<T> head;
     private int count;
 
-    public SinglyLinkedList() {
-    }
-
     public int getSize() {
         return count;
     }
 
     public T getFirstElement() {
+        if (count == 0) {
+            throw new NullPointerException("Список пуст. Невозможно получить первый элемент");
+        }
+
         return head.getData();
     }
 
     public T getElement(int index) {
+        if (count == 0) {
+            throw new NullPointerException("Список пуст. Невозможно получить элемент по индексу");
+        }
+
         if (index >= count || index < 0) {
-            throw new IllegalArgumentException("Список не содержит элемент по указанному индексу");
+            throw new IndexOutOfBoundsException("Список не содержит элемент по указанному индексу");
         }
 
-        ListItem<T> p = head;
-
-        for (int i = 0; i < index; ++i) {
-            p = p.getNext();
-        }
+        ListItem<T> p = findChosenElement(index);
 
         return p.getData();
     }
 
-    public T changeElement(int index, T data) {
+    public T setElement(int index, T data) {
+        if (count == 0) {
+            throw new NullPointerException("Список пуст. Невозможно установить значение элемента по индексу");
+        }
+
         if (index >= count || index < 0) {
-            throw new IllegalArgumentException("Список не содержит элемент по указанному индексу");
+            throw new IndexOutOfBoundsException("Список не содержит элемент по указанному индексу");
         }
 
-        ListItem<T> p = head;
-
-        for (int i = 0; i < index; ++i) {
-            p = p.getNext();
-        }
-
+        ListItem<T> p = findChosenElement(index);
         T temp = p.getData();
         p.setData(data);
 
@@ -47,24 +47,20 @@ public class SinglyLinkedList<T> {
     }
 
     public T removeElement(int index) {
+        if (count == 0) {
+            throw new NullPointerException("Список пуст. Невозможно удалить элемент по индексу");
+        }
+
         if (index >= count || index < 0) {
-            throw new IllegalArgumentException("Список не содержит элемент по указанному индексу");
+            throw new IndexOutOfBoundsException("Список не содержит элемент по указанному индексу");
         }
 
         if (index == 0) {
-            T temp = head.getData();
-            removeFirstElement();
-
-            return temp;
+            return removeFirstElement();
         }
 
-        ListItem<T> p = head;
-        ListItem<T> prev = null;
-
-        for (int i = 0; i < index; ++i) {
-            prev = p;
-            p = p.getNext();
-        }
+        ListItem<T> prev = findChosenAndPreviousElement(index)[0];
+        ListItem<T> p = findChosenAndPreviousElement(index)[1];
 
         T temp = p.getData();
         prev.setNext(p.getNext());
@@ -75,7 +71,7 @@ public class SinglyLinkedList<T> {
 
     public void insertElement(int index, T data) {
         if (index > count || index < 0) {
-            throw new IllegalArgumentException("Список не содержит элемент по указанному индексу");
+            throw new IndexOutOfBoundsException(" Невозможно вставить элемент по указанному индексу. Длина списка меньше введенного значения");
         }
 
         if (index == 0) {
@@ -83,20 +79,17 @@ public class SinglyLinkedList<T> {
             return;
         }
 
-        ListItem<T> p = head;
-        ListItem<T> prev = null;
-
-        for (int i = 0; i < index; ++i) {
-            prev = p;
-            p = p.getNext();
-        }
-
+        ListItem<T> prev = findChosenAndPreviousElement(index)[0];
         ListItem<T> element = new ListItem<>(data, prev.getNext());
         prev.setNext(element);
         ++count;
     }
 
-    public boolean removeValue(T data) {
+    public boolean remove(T data) {
+        if (count == 0) {
+            throw new NullPointerException("Список пуст. Невозможно удалить элемент");
+        }
+
         if (head.getData().equals(data)) {
             removeFirstElement();
 
@@ -105,33 +98,57 @@ public class SinglyLinkedList<T> {
 
         for (ListItem<T> prev = head, p = head.getNext(); p != null; prev = p, p = p.getNext()) {
             if (p.getData().equals(data)) {
-
                 prev.setNext(p.getNext());
                 --count;
 
                 return true;
             }
         }
+
         return false;
     }
 
 
     public void insertFirstElement(T data) {
-        /*ListItem p = new ListItem(data, head);
-        head = p;*/
         head = new ListItem<>(data, head);
         ++count;
     }
 
     public T removeFirstElement() {
+        if (count == 0) {
+            throw new NullPointerException("Список пуст. Невозможно удалить первый элемент");
+        }
+
         T temp = head.getData();
-        head = head.getNext();
+
+        if (count == 1) {
+            head = null;
+        } else {
+            head = head.getNext();
+        }
+
         --count;
 
         return temp;
     }
 
-    public SinglyLinkedList<T> ternList() {
+    public void tern() {
+        if (count == 0) {
+            throw new NullPointerException("Список пуст. Невозможно развернуть список");
+        }
+
+        if (count == 1) {
+            return;
+        }
+
+        int index = 0;
+        for (ListItem<T> p = head; p != null; p = p.getNext()) {
+            ++index;
+            insertFirstElement(removeElement(index));
+        }
+    }
+
+    public SinglyLinkedList<T> getTurn() {
         SinglyLinkedList<T> invertedList = new SinglyLinkedList<>();
         for (ListItem<T> p = head; p != null; p = p.getNext()) {
             invertedList.insertFirstElement(p.getData());
@@ -140,14 +157,18 @@ public class SinglyLinkedList<T> {
         return invertedList;
     }
 
-    public SinglyLinkedList<T> copyList() {
-        SinglyLinkedList<T> copied1List = ternList();
-
-        return copied1List.ternList();
+    public SinglyLinkedList<T> copy() {
+        SinglyLinkedList<T> copiedList = getTurn();
+        copiedList.tern();
+        return copiedList;
     }
 
     @Override
     public String toString() {
+        if (count == 0) {
+            return "{}";
+        }
+
         StringBuilder sb = new StringBuilder();
         sb.append("{");
         ListItem<T> p = head;
@@ -162,5 +183,26 @@ public class SinglyLinkedList<T> {
         sb.append("}");
 
         return sb.toString();
+    }
+
+    private ListItem<T> findChosenElement(int index) {
+        ListItem<T> p = head;
+
+        for (int i = 0; i < index; ++i) {
+            p = p.getNext();
+        }
+        return p;
+    }
+
+    private ListItem<T>[] findChosenAndPreviousElement(int index) {
+        ListItem<T> p = head;
+        ListItem<T> prev = null;
+
+        for (int i = 0; i < index; ++i) {
+            prev = p;
+            p = p.getNext();
+        }
+
+        return new ListItem[]{prev, p};
     }
 }
--- List/src/ru/academit/kalagur/list_main/Main.java
+++ List/src/ru/academit/kalagur/list_main/Main.java
@@ -5,6 +5,7 @@ import ru.academit.kalagur.list.SinglyLinkedList;
 public class Main {
     public static void main(String[] args) {
         SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        System.out.println(list);
         list.insertFirstElement(4);
         list.insertFirstElement(7);
         list.insertFirstElement(15);
@@ -14,39 +15,44 @@ public class Main {
         System.out.println("Размер списка: " + list.getSize());
         //получение значения первого элемента
         System.out.println("Первый элемент списка: " + list.getFirstElement());
-        System.out.println(list.toString());
+        System.out.println(list);
 
         //получение/изменение значения по указанному индексу
         System.out.println("Размер списка:" + list.getSize());
         System.out.println("Элемент списка по индексу: " + list.getElement(3));
-        System.out.println("Изменённый элемент: " + list.changeElement(3, 0));
-        System.out.println(list.toString());
+        System.out.println("Изменённый элемент: " + list.setElement(3, 0));
+        System.out.println(list);
 
         //удаление элемента по индексу
-        Integer x1 = list.removeElement(4);
+        Integer x1 = list.removeElement(0);
         System.out.println("Удаленный элемент: " + x1);
-        System.out.println(list.toString());
+        System.out.println(list);
 
         //вставка элемента по индексу
         list.insertElement(0, 1000);
-        System.out.println(list.toString());
+        System.out.println(list);
 
         //удаление узла по значению
-        boolean delElement = list.removeValue(11);
-        System.out.println("После удаления элемента по значению: " + list.toString());
+        boolean delElement = list.remove(11);
+        System.out.println("После удаления элемента по значению: " + list);
         System.out.println("Элемент удален: " + delElement);
 
         //удаление первого элемента
         Integer x3 = list.removeFirstElement();
-        System.out.println(list.toString());
+        System.out.println(list);
         System.out.println("Элемент удален: " + x3);
 
         //разворот списка
-        SinglyLinkedList<Integer> invertedList = list.ternList();
-        System.out.println("Развернутый массив:" + invertedList.toString());
+        list.removeElement(0);
+        list.removeElement(0);
+        //list.removeElement(0);
+        // list.removeElement(0);
+        System.out.println("Исходный массив:"+ list);
+        list.tern();
+        System.out.println("Развернутый массив:"+ list);
 
         //копирование списка
-        SinglyLinkedList<Integer> copiedList = list.copyList();
-        System.out.println("Новый массив:" + copiedList.toString());
-    }
+        SinglyLinkedList<Integer> copiedList = list.copy();
+        System.out.println("Скопированный массив:" + copiedList);
+   }
 }
